41453c87e992860f3141b31879484b676b37ed74
Nexora712 <nexora712@gmail.com>
2026-02-15 10:53:49 +0530

    final day-3 file

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('hellow world'))
+app.use(express.json())
 
-app.get('/home', (req, res) => res.send('This is home'))
+const notes = []
 
-app.listen(3000)
+app.post("/notes", (req, res) => {
+    console.log(req.body)
+
+    notes.push(req.body)
+
+    res.send("notes created...")
+})
+
+app.get("/notes", (req, res)=>{
+    res.send(notes)
+})
+
+app.listen(3000, ()=>{
+    console.log('The server is running on port 3000...')
+})
